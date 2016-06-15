100d38bce8850643c5b7ca122edd19fdf37c5cc4
TheFeaturedEpic <thefeaturedepic@gmail.com>
2016-06-15 11:54:50 +0100

    Added Internet Explorer Detection

---
+++ ie.js
@@ -0,0 +1,11 @@
+function ie() {
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf("MSIE ");
+
+    if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./)) {
+        retun true;
+    }
+    else {
+        return false;
+    }
+}
